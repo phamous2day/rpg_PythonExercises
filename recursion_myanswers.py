d211c36af2f1774e3fbfe198c35da9db324082dd
David Pham <phamous2day@users.noreply.github.com>
2016-07-24 14:54:10 -0400

    Update recursion_myanswers.py

--- recursion_myanswers.py
+++ recursion_myanswers.py
@@ -1,10 +1,10 @@
 #1
-def print_numbers(start, end):
-    print end
-    if end > 1:
-        print_numbers (start, (end-1))
+def print_numbers(end,start = 1):
+    if start <= end:
+        print start
+        print_numbers (end,start+1)
 
-print_numbers(1, 10)
+print_numbers(10)
 
 
 
@@ -18,3 +18,43 @@ def print_names(names):
         print_names(names - 1)
 
 print_names(list)
+
+
+#Another way
+list = ['Bruce', 'Clark', 'Diana']
+def say_hello(list, i=0):
+    if i< len(list):
+        name = list[i]
+        print "Hello, %s!" %name
+        say_hello(list, i+1)
+
+say_hello(list)
+
+
+#3
+class LLNode(object):
+    def __init__ (self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return "LLNode(% r)" % self.data
+
+one = LLNode(1)
+two = LLNode(2)
+three = LLNode(3)
+four = LLNode(4)
+
+one.next = two
+two.next = three
+three.next = four
+
+def ll_lookup(node, target):
+    if node:
+        if node.data == target:
+            return node
+        else:
+            return ll_lookup(node.next, target)
+print ll_lookup(one, 3)
+print ll_lookup(one, 5)
+print ll_lookup(three, 1)
